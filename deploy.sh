3b8fdca1686b7160d172aa3a37a5aef48c19bf72
uicheung <uicheung@noreply.gitcode.com>
2025-01-16 09:30:16 +0800

    feat: 更新sh文件

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
 set -e
@@ -17,9 +16,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:uicheung/uicheung.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git@github.com:uicheung/uicheung.github.io.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
