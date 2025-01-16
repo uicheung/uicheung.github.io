261593ee1063fd4b641f1e6f490430bc878217ed
uicheung <uicheung@noreply.gitcode.com>
2025-01-16 09:55:14 +0800

    Please

--- deploy.sh
+++ deploy.sh
@@ -3,22 +3,22 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd .vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "deploy $(date '+%Y-%m-%d %H:%M:%S')"
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:uicheung/uicheung.github.io.git master
+# git push -f git@github.com:uicheung/uicheung.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:uicheung/uicheung.github.io.git master:gh-pages
 
 cd -
